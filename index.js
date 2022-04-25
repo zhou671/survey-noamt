8abd05ec26ee13f12945f9f267a2d92b14c62dba
Joe Zhou <joezhou@Joes-MacBook-Pro.local>
2022-04-25 14:07:55 -0700

    delete +2

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function preparefn(seqid, problemset){
     let baselines = ['b1', 'b2', 'b3', 'b4', 'b5'];
     let arr = [];
     let offset = -1;
-    for(let i = 0; i < task_num_per_person + 2; i++){
+    for(let i = 0; i < task_num_per_person; i++){
         if(i % 11 == 0){
             offset += 1
         }
