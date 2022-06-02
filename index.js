aa6e42b47a1e20a4c582c198ce28573b184c2541
Joe Zhou <joezhou@Joes-MacBook-Pro.local>
2022-06-02 01:02:57 -0700

    abc 1

--- index.js
+++ index.js
@@ -7,22 +7,23 @@ const uuidv4 = require("uuid/v4");
 const { kMaxLength } = require('buffer');
 const { off } = require('process');
 
-//const insert_user_record = 'INSERT INTO test.amt50 (workid, uniquecode, problemset) VALUES (?, ?, ((select count(*) from test.amt50 subquery) + 1) % 200)';
+const insert_user_record = 'INSERT INTO abc.amt50 (workid, uniquecode, problemset) VALUES (?, ?, ((select count(*) from abc.amt50 subquery) + 1) % 2)';
 //const insert_user_record = 'INSERT INTO formal2.amt50 (workid, uniquecode, problemset) VALUES (?, ?, ((select count(*) from formal.amt50 subquery) + 1) % 200)';
-const insert_user_record = 'INSERT INTO formal.amt50 (workid, uniquecode, problemset) VALUES (?, ?, ?)';
-const select_user_record = 'SELECT * FROM formal.amt50 where workid = ?'
-const update_seqid = 'UPDATE formal.amt50 SET seqid = ? where workid = ?';
-const insert_choice = 'INSERT INTO formal.choices50 (workid, pid, c) VALUES (?, ?, ?)';
-const insert_comment = 'INSERT INTO formal.feedback (comment) VALUES (?)';
+//const insert_user_record = 'INSERT INTO abc.amt50 (workid, uniquecode, problemset) VALUES (?, ?, ?)';
+const select_user_record = 'SELECT * FROM abc.amt50 where workid = ?'
+const update_seqid = 'UPDATE abc.amt50 SET seqid = ? where workid = ?';
+const insert_choice = 'INSERT INTO abc.choices50 (workid, pid, c) VALUES (?, ?, ?)';
+const insert_comment = 'INSERT INTO abc.feedback (comment) VALUES (?)';
 
-const total_tasks = 10000
+
+const total_tasks = 100
 const task_num_per_person = 55
 const pid_length = 5
 
-const problem_set = new Array(10000);
+const problem_set = new Array(100);
 var good_list = {}
 
-for(var i = 0; i < 10000; i++){
+for(var i = 0; i < 100; i++){
 	problem_set[i] = 0;
 }
 
@@ -73,35 +74,56 @@ function takeFirst(){
 }
 
 function preparefn(seqid, problemset){
-    let base = (problemset) * 50;
-    let baselines = ['b1', 'b2', 'b3', 'b4', 'b5'];
-    let arr = [];
-    let offset = -1;
-    let count = seqid + 1;
-    for(let ite = 0; ite < 6; ite++){
-        if(count == 56){
-            break;
+    // let base = (problemset) * 50;
+    // let baselines = ['b1', 'b2', 'b3', 'b4', 'b5'];
+    // let arr = [];
+    // let offset = -1;
+    // let count = seqid + 1;
+    // for(let ite = 0; ite < 6; ite++){
+    //     if(count == 56){
+    //         break;
+    //     }
+    //     for(let i = 0; i < 100; i++){
+    //         if(count == 56){
+    //             break;
+    //         }
+    //         if(count % 11 == 0){
+    //             offset += 1
+    //             arr.push(baselines[offset])
+    //             console.log(baselines[offset])
+    //             count += 1;
+    //         } else if (problem_set[i] == ite){
+    //             arr.push(i.toString())
+    //             console.log(i.toString())
+    //             count += 1;
+    //             problem_set[i] += 1;
+    //         }
+    //     }
+    // }
+    // arr.push('b1')
+
+    // return arr;
+
+    let base = problem_set * 50;
+    let baselines = ['b1', 'b2', 'b3', 'b4', 'b5']
+    let arr = []
+    let offset = 0;
+    for(let i = 0; i < 55; i++){
+        if((i + 1) % 11 == 0){
+            offset += 1
         }
-        for(let i = 0; i < 10000; i++){
-            if(count == 56){
-                break;
-            }
-            if(count % 11 == 0){
-                offset += 1
-                arr.push(baselines[offset])
-                console.log(baselines[offset])
-                count += 1;
-            } else if (problem_set[i] == ite){
-                arr.push(i.toString())
-                console.log(i.toString())
-                count += 1;
-                problem_set[i] += 1;
-            }
+        if(i < seqid){
+            continue
+        }
+        if((i + 1) % 11 == 0){
+            arr.push(baselines[offset])
+        } else {
+            arr.push((base + i - offset).toString())
         }
     }
+    console.log(arr)
     arr.push('b1')
-
-    return arr;
+    return arr
 }
 
 app.get('/api/checkuser/:id', cors(), (req, res, next) => {
@@ -244,68 +266,70 @@ con.connect(function(err) {
     console.log("Connected!");
 });
 
-const formalselect = "select * from formal.amt50"
-const formalchoice = "select * from formal.choices50"
 
-con.query(formalselect,(err, results, fields)=> {
-	// workid, uniquecode, problemset, seqid
+
+// const formalselect = "select * from formal.amt50"
+// const formalchoice = "select * from formal.choices50"
+
+// con.query(formalselect,(err, results, fields)=> {
+// 	// workid, uniquecode, problemset, seqid
 	
-	var good_list = {}
-	for(var i = 0; i < results.length; i++){
-		var wid = results[i].workid;
-		var sid = results[i].seqid;
-		if(sid == '55'){
-			good_list[wid] = 0 
-		}
-	}
+// 	var good_list = {}
+// 	for(var i = 0; i < results.length; i++){
+// 		var wid = results[i].workid;
+// 		var sid = results[i].seqid;
+// 		if(sid == '55'){
+// 			good_list[wid] = 0 
+// 		}
+// 	}
 
-	const cs = {"b1":0, "b2":0, "b3":0, "b4":0, "b5":0}
-	con.query(formalchoice, (err, results, field) =>{
-		for(var i = 0; i < results.length; i++){
-			var wid = results[i].workid;
-			var pid = results[i].pid;
-			var c = results[i].c;
-			if((wid in good_list) && (c == "1") && (pid in cs)){
-				good_list[wid] += 1;
-			}
-		}
-		for(let k in good_list){
-			if(good_list[k] != 5){
-				delete good_list[k];
-			} else {
-				good_list[k] = 0;
-			}
-		}
-		for(var i = 0; i < results.length; i++){
-			var wid = results[i].workid;
-			var pid = results[i].pid;
-			var c = results[i].c;
-			if((wid in good_list) && (c == '1')){
-				good_list[wid] += 1;
-			}
-		}
-		for(const k in good_list){
-			if(good_list[k] == 55){
-				delete good_list[k];
-			} else {
-				good_list[k] = 0
-			}
-		}
-		for(var i = 0; i < results.length; i++){
-			var wid = results[i].workid;
-			var pid = results[i].pid;
-			var c = results[i].c;
-			if((wid in good_list) && !(pid in cs)){
-				var idx = parseInt(pid);
-				problem_set[idx] += 1;
-			}
-		}
-		var count = 0;
-		for(var i = 0; i < 10000; i++){
-			if(problem_set[i] == 5){
-				count += 1;	
-			}
-		}
-		console.log('The valid count is: ' + count)
-	})
-})
+// 	const cs = {"b1":0, "b2":0, "b3":0, "b4":0, "b5":0}
+// 	con.query(formalchoice, (err, results, field) =>{
+// 		for(var i = 0; i < results.length; i++){
+// 			var wid = results[i].workid;
+// 			var pid = results[i].pid;
+// 			var c = results[i].c;
+// 			if((wid in good_list) && (c == "1") && (pid in cs)){
+// 				good_list[wid] += 1;
+// 			}
+// 		}
+// 		for(let k in good_list){
+// 			if(good_list[k] != 5){
+// 				delete good_list[k];
+// 			} else {
+// 				good_list[k] = 0;
+// 			}
+// 		}
+// 		for(var i = 0; i < results.length; i++){
+// 			var wid = results[i].workid;
+// 			var pid = results[i].pid;
+// 			var c = results[i].c;
+// 			if((wid in good_list) && (c == '1')){
+// 				good_list[wid] += 1;
+// 			}
+// 		}
+// 		for(const k in good_list){
+// 			if(good_list[k] == 55){
+// 				delete good_list[k];
+// 			} else {
+// 				good_list[k] = 0
+// 			}
+// 		}
+// 		for(var i = 0; i < results.length; i++){
+// 			var wid = results[i].workid;
+// 			var pid = results[i].pid;
+// 			var c = results[i].c;
+// 			if((wid in good_list) && !(pid in cs)){
+// 				var idx = parseInt(pid);
+// 				problem_set[idx] += 1;
+// 			}
+// 		}
+// 		var count = 0;
+// 		for(var i = 0; i < 100; i++){
+// 			if(problem_set[i] == 5){
+// 				count += 1;	
+// 			}
+// 		}
+// 		console.log('The valid count is: ' + count)
+// 	})
+// })
